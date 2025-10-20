286c1742f7751ac93a1ba46dca2291f812787b01
Ankita562 <ankitaguptaa989@gmail.com>
2025-10-20 13:10:47 +0530

    feat: Add microservices architecture diagram (closes #2)

---
+++ microservices-architecture.py
@@ -0,0 +1,22 @@
+from diagrams import Diagram, Cluster
+from diagrams.aws.network import APIGateway
+from diagrams.aws.compute import Lambda
+from diagrams.aws.database import DDB
+
+with Diagram("Microservices Architecture", show=False):
+    # This is the entry point for users
+    api_gateway = APIGateway("API Gateway")
+
+    # This is a group of services
+    with Cluster("Services"):
+        user_service = Lambda("User Service")
+        order_service = Lambda("Order Service")
+
+    # This is a group of databases
+    with Cluster("Databases"):
+        user_db = DDB("User DB")
+        order_db = DDB("Order DB")
+
+    # This is how you connect them
+    api_gateway >> user_service >> user_db
+    api_gateway >> order_service >> order_db
